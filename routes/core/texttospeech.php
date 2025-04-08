2e245973c819279fc77c38ad14c7f0b4d5fda33b
Moises_Salazar <moises.salazar@ucsp.edu.pe>
2025-04-08 08:35:07 -0500

    Vistas

--- routes/core/texttospeech.php
+++ routes/core/texttospeech.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\Core\TextToSpeechController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     // Ruta para mostrar el índice de Text-to-Speech
-    Route::get('text-to-speech', [TextToSpeechController::class, 'index'])->name('texttospeech.index');
+    Route::get('text-to-speech', [TextToSpeechController::class, 'index'])->name('texttospeech.show');
 });
